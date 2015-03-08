4b47b3d75b313dd9ce9c9e7b56badcfe1f6ce88a
fff <milel.f@gmail.com>
2015-03-08 18:40:19 +0800

    Structuring an App

---
+++ ToDoList/AddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddToDoItemViewController.h
+//  ToDoList
+//
+//  Created by Phillip on 3/8/15.
+//  Copyright (c) 2015 Phillip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddToDoItemViewController : UIViewController
+
+@end
---
+++ ToDoList/ToDoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by Phillip on 3/8/15.
+//  Copyright (c) 2015 Phillip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToDoListTableViewController : UITableViewController
+    - (IBAction)unwindToList:(UIStoryboardSegue *)segue;
+@end
