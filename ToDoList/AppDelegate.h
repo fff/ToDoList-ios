c0e2b1703fa2cc2e4759be7a62a745ce9aa4a237
fff <milel.f@gmail.com>
2015-03-08 13:52:47 +0800

    Tutorial: Basics

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Phillip on 3/7/15.
+//  Copyright (c) 2015 Phillip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ToDoList
+//
+//  Created by Phillip on 3/7/15.
+//  Copyright (c) 2015 Phillip. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
